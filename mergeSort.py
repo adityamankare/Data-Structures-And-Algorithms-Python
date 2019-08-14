f8b88cafe2c9457ef702d28a73295563e09e225c
Aditya Mankare <adityamankare@gmail.com>
2019-08-14 15:44:51 -0500

    Update mergeSort.py

--- mergeSort.py
+++ mergeSort.py
@@ -1,61 +1,22 @@
-#MERGE SORT
-
-def splitList(inputList):
-  lengthOfList = len(inputList)
-  midpoint = lengthOfList//2
-  leftList = inputList[:midpoint]
-  rightList = inputList[midpoint:]
-  return leftList,rightList
-
-
-def mergeSortedLists(leftList, rightlist):
-  leftList, rightList = splitList(inputList)
-  #special case: if one or both lists are empty
-  if (len(leftList) == 0):
-    return rightlist
-  elif (len(rightlist) ==0):
-    return leftList
-  #general case
-  leftIndex = rightIndex = 0
-  mergedList = []
-  targetLengthOfList = len(leftList) + len(rightlist)
-  while (len(mergedList) < targetLengthOfList):
-    if (leftList[leftIndex] <= rightList[rightIndex]):
-      mergedList.append(leftList[leftIndex])
-      leftIndex += 1
-    else:
-      mergedList.append(rightList[rightIndex])
-      rightIndex += 1
-
-    #extreme case: if we are at one end of the list
-    if (rightIndex == len(rightList)):
-      mergedList += leftList[leftIndex:]
-      break
-    elif(leftIndex == len(leftList)):
-      mergedList += rightList[rightIndex:]
-      break
-  return mergedList
-
-
-def mergeSortAlgorithm(inputList):
-  if (len(inputList) <= 1):
+def mergeSort(inputList):
+  if (len(inputList) < 2):
     return inputList
-  else:
-    left,right = splitList(inputList) 
-    return mergeSortedLists(mergeSortAlgorithm(left), mergeSortAlgorithm(right))
-  
 
+  result = []
+  midpoint = int(len(inputList)/2)
 
+  left = mergeSort(inputList[:midpoint])
+  right = mergeSort(inputList[midpoint:])
 
-inputList = input().split(',')
-#left, right = splitList(inputList)
-#print(left)
-#print(right)
+  while (len(left) > 0) and (len(right) > 0):
+    if (left[0] > right[0]):
+      result.append(right.pop(0))   
+    else:
+      result.append(left.pop(0))
 
-#mergedListVariable = mergeSortedLists(splitList(inputList))
-#print('This is the merged list: ', mergedListVariable)
+  result.extend(left + right)
+  return result
 
-mergeSortAlgorithm(inputList)
-print(mergeSortAlgorithm(inputList))
-#print('This is the result', result)
+inputList = list(map(int, input().split(',')))
 
+print(mergeSort(inputList))
