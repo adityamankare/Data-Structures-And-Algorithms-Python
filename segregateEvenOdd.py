2330d96041172826dc753262e193db1abd9b114d
Aditya Mankare <adityamankare@gmail.com>
2019-09-28 17:55:33 -0500

    Create segregateEvenOdd.py

---
+++ segregateEvenOdd.py
@@ -0,0 +1,18 @@
+def segregateEvenOdd(arr):
+  left = 0
+  right = len(arr) - 1
+  while left < right:
+    while arr[left]%2==0 and left < right:
+      left += 1
+    while arr[right]%2!=0 and left < right:
+      right -= 1
+    if left < right:
+      arr[left], arr[right] = arr[right], arr[left]
+      left += 1
+      right -= 1
+  return arr
+
+print(segregateEvenOdd([12, 34, 45, 9, 8, 90, 3]))
+
+O(n) time
+O(1) space
